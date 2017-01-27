d27cc3c09c1fa5e1be3474fabad18a83f3c767d8
Ethan Guo <ethguo@gmail.com>
2017-01-27 14:21:01 -0500

    Latest code from Codepen, compiled using Gulp

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var ease = {
@@ -10,7 +12,6 @@ var ease = {
     return -Math.pow(t - 1, 4) + 1;
   } };
 
-//TODO: Try InOut (http://gizma.com/easing/)
 var mathx = {
   clamp: function clamp(x, min, max) {
     return Math.min(Math.max(x, min), max);
@@ -21,76 +22,79 @@ var mathx = {
 };
 
 var GooeyTransition = function () {
-  function GooeyTransition(paths) {
+  function GooeyTransition(svg) {
     _classCallCheck(this, GooeyTransition);
 
-    this.paths = paths;
+    this.svg = $(svg);
+    this.paths = $(svg).find("path");
     this.limit = 0;
     this.gap = 0.05;
   }
 
-  GooeyTransition.prototype.getPath = function getPath(t) {
-    var easeQuad = mathx.scale(ease.quadraticOut(mathx.clamp(t, 0, 1)), 1, 0, this.limit, 1);
-    var easeQuart = mathx.scale(ease.quarticOut(mathx.clamp(t, 0, 1)), 1, 0, this.limit, 1);
-    // var ease = easeQuad / easeQuart;
-    return "\n      M 0 0\n      V " + easeQuart + "\n      Q 0.125 " + easeQuart + " 0.25 " + easeQuad + "\n      T 0.5 " + easeQuad + "\n      T 0.75 " + easeQuad + "\n      T 1 " + easeQuart + "\n      V 0\n    ";
-  };
-
-  GooeyTransition.prototype.render = function render(t) {
-    for (var i = 0; i < this.paths.length; i++) {
-      this.paths[i].attr('d', this.getPath(t + i * this.gap));
+  _createClass(GooeyTransition, [{
+    key: "getPath",
+    value: function getPath(easeQuad, easeQuart) {
+      return "\n      M 0 0\n      V " + easeQuart + "\n      Q 12.5 " + easeQuart + " 25 " + easeQuad + "\n      T 50 " + easeQuad + "\n      T 75 " + easeQuad + "\n      T 100 " + easeQuart + "\n      V 0\n    ";
     }
-  };
+  }, {
+    key: "render",
+    value: function render(t) {
+      var easeQuad = mathx.scale(ease.quadraticOut(t), 1, 0, this.limit, $(window).height());
+      var easeQuart = mathx.scale(ease.quarticOut(t), 1, 0, this.limit, $(window).height());
+      var easeQuad2 = mathx.scale(ease.quadraticOut(t + this.gap), 1, 0, this.limit, $(window).height());
+      var easeQuart2 = mathx.scale(ease.quarticOut(t + this.gap), 1, 0, this.limit, $(window).height());
+
+      var svgHeight = easeQuad + easeQuad - easeQuart;
+      this.svg.css("height", svgHeight);
+      this.svg.attr("viewBox", "0 0 100 " + svgHeight);
+
+      $(this.paths[0]).attr('d', this.getPath(easeQuad, easeQuart));
+      $(this.paths[1]).attr('d', this.getPath(easeQuad2, easeQuart2));
+    }
+  }]);
 
   return GooeyTransition;
 }();
 
-var landing1 = $("#gooey-bg #landing1");
-var landing2 = $("#gooey-bg #landing2");
-// var footer1 = $("#gooey-bg #footer1");
-// var footer2 = $("#gooey-bg #footer2");
-
-var landingTransition = new GooeyTransition([landing1, landing2]);
+var landingTransitionScale = 1.5;
+var landingTransition = new GooeyTransition("#gooey-bg");
 // const footerTransition = new GooeyTransition(0.9, [footer1, footer2]);
 
 // Update anytime page is scrolled.
 $(window).scroll(function () {
-  console.log($(".active").attr("href"));
+  // console.log($(".active").attr("href"));
 
   var t = $(window).scrollTop() / $(window).height();
-  // console.log($(window).height(), t)
 
-  landingTransition.render(t * 2);
+  landingTransition.render(mathx.clamp(t * landingTransitionScale, 0, 1));
 
   // footerTransition.render(t*2 - 4);
 
   // Rescale landing
-  var landingHeight = mathx.scale(mathx.clamp(t, 0, 0.5), 0.5, 0, 0, $(this).height());
+  var landingHeight = mathx.scale(mathx.clamp(t, 0, 1 / landingTransitionScale), 1 / landingTransitionScale, 0, 0, $(this).height());
   $("#landing").css("height", landingHeight);
 
+  // Fade in/out nav-header
   if ($("#nav-header").queue().length == 0 && t > 0.49) {
-    // console.log("fading in");
     $("#nav-header").fadeIn(200);
   } else if ($("#nav-header").queue().length == 0 && t < 0.5) {
-    // console.log("fading out");
     $("#nav-header").fadeOut(200);
   }
 });
 
 $(window).resize(function () {
-  landingTransition.limit = $("#nav-header").height() / $(window).height();
+  landingTransition.limit = $("#nav-header").height();
+
   $(this).scroll(); // Trigger a scroll update
 }).resize();
 
-function navScrollTo(tag) {
+$(".nav-link").click(function () {
+  var target = $(this).attr("href");
+  console.log("target", target);
   $("html,body").stop().animate({
-    scrollTop: $(tag).offset().top
+    scrollTop: $(target).offset().top
   }, 400, "easeInOutQuad");
   return false;
-}
+});
 
 $("body").scrollspy({ target: "#nav-header" });
-
-/* TODO *
- * Fix svg z-indexing
- */
