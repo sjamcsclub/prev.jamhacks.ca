89ee2968ec84393596c02590d73c3fa2db1f076d
Ethan Guo <ethguo@gmail.com>
2017-02-08 02:38:34 -0500

    Updated everything

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,8 @@ const mathx = {
   }
 }
 
+// Test if we're on mobile
+const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
 
 class GooeyTransition {
   constructor(svg) {
@@ -42,23 +44,29 @@ class GooeyTransition {
     var easeQuart2 = mathx.scale(ease.quarticOut(t + this.gap), 1, 0, this.limit, $(window).height());
     
     var svgHeight = easeQuad + easeQuad - easeQuart;
-    this.svg.css("height", svgHeight);
+    if (isMobile) {
+      this.svg.css("height", svgHeight*1.1);
+    } else {
+      this.svg.css("height", svgHeight);
+    }
+
     this.svg.attr("viewBox", `0 0 100 ${svgHeight}`);
     
-    $(this.paths[0]).attr('d', this.getPath(easeQuad, easeQuart));
-    $(this.paths[1]).attr('d', this.getPath(easeQuad2, easeQuart2));
+    $(this.paths[0]).attr("d", this.getPath(easeQuad, easeQuart));
+    $(this.paths[1]).attr("d", this.getPath(easeQuad2, easeQuart2));
   }
 }
 
 const landingTransitionScale = 1.5;
 const landingTransition = new GooeyTransition("#gooey-bg");
+
 // const footerTransition = new GooeyTransition(0.9, [footer1, footer2]);
 
 // Update anytime page is scrolled.
 $(window).scroll(function() {
   // console.log($(".active").attr("href"));
   
-  var t = $(window).scrollTop() / $(window).height();
+  var t = $(window).scrollTop() / screen.height;
   
   landingTransition.render(mathx.clamp(t*landingTransitionScale, 0, 1));
   
@@ -69,28 +77,69 @@ $(window).scroll(function() {
   $("#landing").css("height", landingHeight);
   
   // Fade in/out nav-header
-  if ($("#nav-header").queue().length == 0 && t > 0.49) {
-    $("#nav-header").fadeIn(200);
+  if ($("#nav-header").queue().length === 0) {
+    if (t > 0.49) {
+      $("#nav-header").fadeIn(200);
+    }
+    else if (t < 0.5) {
+      $("#nav-header").fadeOut(200);
+    }
   }
-  else if ($("#nav-header").queue().length == 0 && t < 0.5) {
-    $("#nav-header").fadeOut(200);
+  if (isMobile) {
+    if ($("#gooey-bg").queue().length === 0) {
+      if (t > 0.5) {
+        $("#gooey-bg").fadeOut(300);
+      }
+      else if (t < 0.49) {
+        $("#gooey-bg").fadeIn(200);
+      }
+    }
   }
-  
 });
 
 $(window).resize(function() {
   landingTransition.limit = $("#nav-header").height();
-  
+  if (isMobile) {
+    landingTransition.limit += $(window).height()/11.6;
+  }
+
+  var scrollSpyOffset = $("#nav-header").height() + parseInt($("section").css("margin-bottom")) / 2;
+  $("body").attr("data-offset", scrollSpyOffset);
+
   $(this).scroll(); // Trigger a scroll update
 }).resize();
 
 $(".nav-link").click(function() {
   var target = $(this).attr("href");
+  var offset = ($("#nav-header").height() + parseInt($("section").css("margin-bottom"))) / 2;
   console.log("target", target);
   $("html,body").stop().animate({
-    scrollTop: $(target).offset().top
+    scrollTop: $(target).offset().top - offset
   }, 400, "easeInOutQuad");
   return false;
 });
 
-$("body").scrollspy({target: "#nav-header"})
+$(function() {
+  if (isMobile) {
+    $("#gooey-bg").css("top", "-10%");
+  }
+});
+
+// On DOM ready
+// $(function(){
+
+//   var $w = $(window),
+//       $background = $("#gooey-bg");
+
+//   // Fix background image jump on mobile
+//   if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+//     $background.css({"top": "auto", "bottom": 0});
+
+//     $w.resize(sizeBackground);
+//     sizeBackground();
+//   }
+
+//   function sizeBackground() {
+//      $background.height(screen.height);
+//   }
+// });
