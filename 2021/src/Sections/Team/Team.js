a5c25ccdb98f0a4544eb2df64b3e2d1772211e2a
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-10-04 23:07:51 -0400

    team section finished

--- 2021/src/Sections/Team/Team.js
+++ 2021/src/Sections/Team/Team.js
@@ -2,11 +2,33 @@ import React from "react";
 
 import "./Team.css";
 
+import Placeholder from "../../Assets/Placeholder.png"
+
 const Team = () => {
   return (
     <div className="team-display-div">
       <div className="team-display-title-div large-title" data-aos="fade-up">
         Meet the Team
+      </div>
+      <div className="team-display-content-div">
+        {
+          ["","","","","","","","","","","",""].map(() => {
+            return (
+              <div className="team-display-content-item">
+                <img className="team-display-headshot" src={Placeholder} ></img>
+                <div className="team-display-content-item-main">Cristian Bicheru</div>  
+                <div className="team-display-content-item-second">Lead Organizer</div>
+              </div>
+            )
+          })
+        }
+        
+        
+
+
+
+
+
       </div>
     </div>
   );
