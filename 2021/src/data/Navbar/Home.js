d906c3b2c96a8256751b4a647ed18b13896d1651
Kevin <kevingao2003@gmail.com>
2021-03-04 21:59:06 -0500

    test

--- 2021/src/data/Navbar/Home.js
+++ 2021/src/data/Navbar/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import Env from '../env.js';
 
 const HomeNavData = [
@@ -30,39 +29,6 @@ const HomeNavData = [
   {
     label: 'Contact',
     id: 'contact-section',
-=======
-import Env from "../env.js";
-
-const HomeNavData = [
-  {
-    label: "About",
-    id: "about-section",
-    enabled: true,
-  },
-  {
-    label: "Schedule",
-    id: "schedule-section",
-    enabled: Env.schedule,
-  },
-  {
-    label: "Sponsors",
-    id: "sponsor-section",
-    enabled: true,
-  },
-  {
-    label: "FAQ",
-    id: "faq-section",
-    enabled: true,
-  },
-  {
-    label: "Team",
-    id: "team-section",
-    enabled: true,
-  },
-  {
-    label: "Contact",
-    id: "contact-section",
->>>>>>> Stashed changes
     enabled: true,
   },
 ];
