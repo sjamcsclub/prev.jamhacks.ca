b84d241cd14fbafb14029ad90711b876b487d01c
Kevin <kevingao2003@gmail.com>
2021-03-04 22:01:27 -0500

    try and fix folder issue

--- 2021/src/data/socials.js
+++
@@ -1,8 +0,0 @@
-const Socials = {
-  instagram: 'https://www.instagram.com/jamhacks',
-  facebook: 'https://www.facebook.com/JAMHacks',
-  linkedin: 'https://www.linkedin.com/company/jamhacks/',
-  twitter: 'https://twitter.com/jam_hacks?lang=en',
-};
-
-export default Socials;
