810849f37bbfa75cb5b05bd3996f2bded4bfcb22
Kevin <kevingao2003@gmail.com>
2021-05-01 20:06:19 -0400

    add blog graphic

--- 2021/src/pages/Home/Blog/index.js
+++ 2021/src/pages/Home/Blog/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import { Header, Text } from '../../../components/Typography';
 import Socials from '../../../data/socials';
+import { ReactComponent as BlogImage } from '../../../assets/undraw/Blog.svg';
 
 const Blog = () => {
   return (
     <Container id="blog-section">
-      <Header>Blog</Header>
-      <Text>
-        Check out our{' '}
-        <a href={Socials.medium} target="_blank" rel="noopener noreferrer">
-          blog posts on Medium,
-        </a>{' '}
-        written by our organizers for fun and insightful articles on hackathons,
-        coding, success stories, and more! We will be posting every week,
-        starting April 26th!
-      </Text>
+      <TextContainer data-aos="fade-up">
+        <Header>Blog</Header>
+        <Text>
+          Interested in learning more about hackathons? Check out our blog posts
+          on the{' '}
+          <a href={Socials.medium} target="_blank" rel="noopener noreferrer">
+            JAMHacks Medium,
+          </a>{' '}
+          , where you'll find articles written by our very own organizers,
+          covering topics related to making your hackathon project stand out,
+          frontend and backend development, impostor syndrome in the tech
+          industry, organizing your own hackathon, coding success stories, and
+          more! We will be posting every week, starting April 26th!
+        </Text>
+      </TextContainer>
+      <ImageContainer data-aos="fade-up">
+        <StyledBlogImage>
+          <BlogImage />
+        </StyledBlogImage>
+      </ImageContainer>
     </Container>
   );
 };
@@ -23,6 +34,38 @@ const Blog = () => {
 const Container = styled.div`
   background: #fbf9ff;
   padding: 0 10%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const TextContainer = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-width: 380px;
+  margin: 1em 0;
+`;
+
+const ImageContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-width: 380px;
+  margin: 1em 0;
+`;
+
+const StyledBlogImage = styled.div`
+  justify-content: center;
+  display: flex;
+  & svg {
+    width: 80%;
+    height: auto;
+  }
 `;
 
 export default Blog;
