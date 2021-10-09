d227347c610b499d8f408185ec718e799e96c90b
Daniel Yu <55118464+DanielYu2004@users.noreply.github.com>
2021-10-09 18:45:06 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e244291632d52911aa39b3ae002a94a5.js"
+  "/precache-manifest.b87d4d35612068d4ce9be15efefed887.js"
 );
 
 self.addEventListener('message', (event) => {
