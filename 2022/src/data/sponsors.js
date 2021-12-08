1c91cb6118d75130532ad63011f95f8d8cb26a76
Daniel Yu <55118464+DanielYu2004@users.noreply.github.com>
2021-12-08 17:51:34 -0500

    update sponsors

--- 2022/src/data/sponsors.js
+++ 2022/src/data/sponsors.js
@@ -1,150 +1,51 @@
-import ETH from '../assets/sponsorlogos/blueberry/ETH.svg';
-import QuantStamp from '../assets/sponsorlogos/blueberry/Quantstamp.png';
+import TMMC from '../assets/sponsorlogos/blueberry/TMMC.png';
 
-import UW from '../assets/sponsorlogos/strawberry/UW.png';
-import CockroachDB from '../assets/sponsorlogos/strawberry/CockroachDB.svg';
+import HestiaAcademy from '../assets/sponsorlogos/strawberry/HestiaAcademy.png';
 
-import SweaterPlanet from '../assets/sponsorlogos/grape/SweaterPlanet.png';
-import Museum from '../assets/sponsorlogos/grape/Museum.png';
-import DigitalOcean from '../assets/sponsorlogos/grape/DigitalOcean.svg';
-
-import CloudFlare from '../assets/sponsorlogos/peach/Cloudflare.png';
-import WolframAlpha from '../assets/sponsorlogos/peach/WolframAlpha.png';
-import Linode from '../assets/sponsorlogos/peach/Linode.png';
-import CtrlV from '../assets/sponsorlogos/peach/CtrlV.png';
-import Password from '../assets/sponsorlogos/peach/1Password.png';
-import MSU from '../assets/sponsorlogos/peach/MSU.png';
-import Balsamiq from '../assets/sponsorlogos/peach/Balsamiq.svg';
-import Framer from '../assets/sponsorlogos/peach/Framer.svg';
-import Axure from '../assets/sponsorlogos/peach/Axure.png';
-import Repl from '../assets/sponsorlogos/peach/Replit.svg';
+import MapleSoft from '../assets/sponsorlogos/peach/MapleSoft.png';
 import Bestbuy from '../assets/sponsorlogos/peach/Bestbuy.png';
 import RStudio from '../assets/sponsorlogos/peach/RStudio.svg';
-import QCSYS from '../assets/sponsorlogos/peach/QCSYS.png';
-import EchoAR from '../assets/sponsorlogos/peach/EchoAR.png';
-import VolareLabs from '../assets/sponsorlogos/peach/VolareLabs.png';
-import Netlify from '../assets/sponsorlogos/peach/Netlify.png';
-
-import Voiceflow from '../assets/sponsorlogos/peach/Voiceflow.png';
-import Magoosh from '../assets/sponsorlogos/peach/Magoosh.png';
-import MapleSoft from '../assets/sponsorlogos/peach/MapleSoft.png';
-import TheCodex from '../assets/sponsorlogos/peach/TheCodex.svg';
+import Echo3d from '../assets/sponsorlogos/peach/Echo3d.png';
+import InterviewCake from '../assets/sponsorlogos/peach/InterviewCake.png';
 
 import HackTheNorth from '../assets/sponsorlogos/partners/HackTheNorth.png';
 
 const blueberry = [
   {
-    link: 'https://ethereum.foundation/',
-    logo: ETH,
-  },
-  {
-    link: 'https://quantstamp.com/',
-    logo: QuantStamp,
+    link: 'https://www.hestia.academy/',
+    logo: TMMC,
   },
 ];
 
 const strawberry = [
   {
-    link: 'https://www.uwaterloo.ca/',
-    logo: UW,
-  },
-  {
-    link: 'https://www.cockroachlabs.com/',
-    logo: CockroachDB,
+    link: 'https://www.hestia.academy/',
+    logo: HestiaAcademy,
   },
 ];
 
-const grape = [
-  {
-    link: 'https://www.sweaterplanet.com/',
-    logo: SweaterPlanet,
-  },
-  {
-    link: 'https://www.themuseum.ca/',
-    logo: Museum,
-  },
-  {
-    link: 'https://www.digitalocean.com/',
-    logo: DigitalOcean,
-  },
-];
+const grape = [];
 
 const peach = [
-  {
-    link: 'https://www.netlify.com/',
-    logo: Netlify,
-  },
-  {
-    link: 'https://www.framer.com/',
-    logo: Framer,
-  },
-  {
-    link: 'https://repl.it/',
-    logo: Repl,
-  },
-  {
-    link: 'https://www.wolfram.com/',
-    logo: WolframAlpha,
-  },
-  {
-    link: 'https://www.linode.com/',
-    logo: Linode,
-  },
-  {
-    link: 'https://www.ctrlv.ca/',
-    logo: CtrlV,
-  },
-  {
-    link: 'https://www.1password.com/',
-    logo: Password,
-  },
-  {
-    link: 'https://www.msumcmaster.ca/',
-    logo: MSU,
-  },
-  {
-    link: 'https://www.balsamiq.com/',
-    logo: Balsamiq,
-  },
-  {
-    link: 'https://www.axure.com/',
-    logo: Axure,
-  },
   {
     link: 'https://www.bestbuy.ca/',
     logo: Bestbuy,
   },
   {
-    link: 'https://uwaterloo.ca/institute-for-quantum-computing/programs/qcsys',
-    logo: QCSYS,
-  },
-  {
-    link: 'https://www.echoar.xyz/',
-    logo: EchoAR,
-  },
-  {
-    link: 'https://www.volarelabs.io/',
-    logo: VolareLabs,
+    link: 'https://www.echo3d.co/',
+    logo: Echo3d,
   },
   {
     link: 'https://www.rstudio.com/',
     logo: RStudio,
   },
   {
-    link: 'https://www.voiceflow.com/',
-    logo: Voiceflow,
-  },
-  {
-    link: 'https://magoosh.com/',
-    logo: Magoosh,
-  },
-  {
     link: 'https://www.maplesoft.com/',
     logo: MapleSoft,
   },
   {
-    link: 'https://thecodex.me/',
-    logo: TheCodex,
+    link: 'https://www.maplesoft.com/',
+    logo: InterviewCake,
   },
 ];
 
