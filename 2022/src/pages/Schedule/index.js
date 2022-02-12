21682268a9ec138b3b9bb3c2c4534147cc6f7e66
Andrew Yang <andy.tubeee@gmail.com>
2022-02-11 22:24:01 -0500

    schedule styles

--- 2022/src/pages/Schedule/index.js
+++ 2022/src/pages/Schedule/index.js
@@ -5,57 +5,22 @@ import { Day1Data } from './data/day1';
 import { Day2Data } from './data/day2';
 import { Day3Data } from './data/day3';
 import './style.css';
+import { useEffect } from 'react';
 
 const Schedule = () => {
   const history = useHistory();
   const [pageNum, setPageNum] = useState(1);
   return (
     <div style={{ zIndex: 2, height: '100vh' }}>
-      <button onClick={() => history.push('/')}>Go Back to Main Page</button>
-      <h1 style={{ color: 'purple', textAlign: 'center', marginTop: '20px' }}>
+      <button className="go_back_btn" onClick={() => history.push('/')}>
+        Go Back to Main Page
+      </button>
+      <h1 style={{ color: '#7B0EF6', textAlign: 'center', marginTop: '20px' }}>
         Schedule Page
       </h1>
-      <MaterialTable
-        columns={[
-          {
-            title: 'Time',
-            field: 'time',
-            cellStyle: {
-              backgroundColor: '#039be5',
-              color: '#FFF',
-            },
-            searchable: false,
-          },
-          { title: 'Event', field: 'title' },
-        ]}
-        data={(() => {
-          switch (pageNum) {
-            case 1:
-              return Day1Data;
-            case 2:
-              return Day2Data;
-            case 3:
-              return Day3Data;
-            default:
-              return [];
-          }
-        })()}
-        onChangePage={(page) => setPageNum(page)}
-        title={`JAMHacks 6 - Day ${pageNum}`}
-        options={{
-          headerStyle: {
-            backgroundColor: '#01579b',
-            color: '#FFF',
-          },
-          exportButton: true,
-          sorting: true,
-          paginationType: 'stepped',
-          paging: false,
-        }}
-      />
       <div className="schedule_btn_row">
         <button
-          className="schedule_btn"
+          className={`schedule_btn ${pageNum === 1 && 'active'}`}
           onClick={() => {
             setPageNum(1);
           }}
@@ -63,7 +28,7 @@ const Schedule = () => {
           Day 1
         </button>
         <button
-          className="schedule_btn"
+          className={`schedule_btn ${pageNum === 2 && 'active'}`}
           onClick={() => {
             setPageNum(2);
           }}
@@ -71,13 +36,63 @@ const Schedule = () => {
           Day 2
         </button>
         <button
-          className="schedule_btn"
+          className={`schedule_btn ${pageNum === 3 && 'active'}`}
           onClick={() => {
             setPageNum(3);
           }}
         >
           Day 3
         </button>
+      </div>
+      <div className="schedule-table-container">
+        {' '}
+        <MaterialTable
+          columns={[
+            {
+              title: 'Time',
+              field: 'time',
+              cellStyle: {
+                backgroundColor: '#EEDFFF',
+                color: '#7B0EF6',
+              },
+              width: '4%',
+              searchable: false,
+            },
+            { title: 'Event', field: 'title' },
+          ]}
+          data={(() => {
+            switch (pageNum) {
+              case 1:
+                return Day1Data;
+              case 2:
+                return Day2Data;
+              case 3:
+                return Day3Data;
+              default:
+                return [];
+            }
+          })()}
+          onChangePage={(page) => setPageNum(page)}
+          title={`JAMHacks 6 - Day ${pageNum}`}
+          options={{
+            showTitle: false,
+            headerStyle: {
+              backgroundColor: '#7B0EF6',
+              color: '#FFF',
+            },
+            rowStyle: {
+              overflowWrap: 'break-word',
+              fontFamily: 'Poppins',
+            },
+            exportButton: false,
+            sorting: true,
+            paginationType: 'stepped',
+            paging: false,
+            tableLayout: 'auto',
+            search: false,
+            toolbar: false,
+          }}
+        />
       </div>
     </div>
   );
