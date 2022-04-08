75786635e4e8d428162b2f0e9827bb3e793731f6
Andrew Yang <andy.tubeee@gmail.com>
2022-04-08 19:38:37 -0400

    responsive map element

--- 2022/src/pages/Home/Venue/index.js
+++ 2022/src/pages/Home/Venue/index.js
@@ -4,14 +4,14 @@ import { Collapse } from 'antd';
 import { media } from '../../../utils/media';
 
 import Header from '../../../components/Typography/Header';
-import { motion } from 'framer-motion';
-import { speakers } from '../../../data/speakers';
+
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
   Marker,
 } from 'react-google-maps';
+import useWindowDimensions from '../../../utils/useWindowDimensions';
 
 const API_KEY = process.env.REACT_APP_GMAP_API_KEY;
 
@@ -20,7 +20,7 @@ const MapComponent = withScriptjs(
     return (
       <GoogleMap
         defaultZoom={15}
-        defaultCenter={{ lat: 43.4716, lng: -80.529678 }}
+        defaultCenter={{ lat: 43.4756, lng: -80.529678 }}
       >
         <Marker position={{ lat: 43.47496, lng: -80.529678 }} />
       </GoogleMap>
@@ -29,18 +29,50 @@ const MapComponent = withScriptjs(
 );
 
 const VenueSection = ({}) => {
+  const { width, height } = useWindowDimensions();
+  const isMobile = width < 768;
   return (
     <Container id="venue-info-section">
       <Header data-aos="fade-up">Venue</Header>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '80px' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '80px',
+          flexWrap: 'wrap',
+          alignContent: 'center',
+        }}
+      >
         <MapComponent
           googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
           loadingElement={<div style={{ height: `100px` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
-          mapElement={<div style={{ height: `600px`, width: '700px' }} />}
+          containerElement={
+            <div
+              style={{
+                height: `300px`,
+                justifySelf: 'center',
+              }}
+            />
+          }
+          mapElement={
+            <div
+              style={{
+                height: `100%`,
+                width: isMobile ? '400px' : '500px',
+                justifySelf: 'center',
+              }}
+            />
+          }
         />
 
-        <p style={{ fontSize: '1.2rem' }}>
+        <p
+          style={{
+            fontSize: '1.2rem',
+            textAlign: 'center',
+            width: isMobile ? '70%' : '40%',
+          }}
+        >
           <strong>Location</strong>: Lazaridis School of Business and Economics,
           64 University Ave W, Waterloo, ON N2L 3C7
         </p>
